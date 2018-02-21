144f7746008c47df10bc9788d9da3ac04ca405bb
Marcio Santiago <marcio704@yahoo.com.br>
2018-02-20 21:04:43 -0300

    Fixing URL for pagination

--- src/reducers/movies.js
+++ src/reducers/movies.js
@@ -2,7 +2,7 @@ import Reactotron from 'reactotron-react-native';
 import axios from 'axios';
 
 import { REQUEST_IS_LOADING, REQUEST_FETCH_DATA_SUCCESS, REQUEST_HAS_ERRORED, FILTER_BY_TITLE, GET_NEXT_MOVIES_PAGE_FROM_API  } from 'movies/src/actions/types';  
-import { MOVIES_API_URL, MOVIES_API_URL_2, MOVIES_API_URL_3 } from 'movies/src/settings';
+import { MOVIES_API_URL } from 'movies/src/settings';
 import { requestFetchDataSuccess, requestHasErrored, requestIsLoading } from 'movies/src/actions/movies';
 
 initialState = {
@@ -52,9 +52,8 @@ export default function moviesReducer(state=initialState, action) {
             }
         case GET_NEXT_MOVIES_PAGE_FROM_API:
             const next_page = state.page + 1;
-           
-            // As mocky.io doesnt accept dynamic body or url parameter for simulating pagination, it was necessary to create differente urls
-            url = next_page === 1 ? MOVIES_API_URL : next_page === 2 ?  MOVIES_API_URL_2 : MOVIES_API_URL_3
+            url = MOVIES_API_URL + '?page=' + next_page
+            
             axios.get(url)
             .then((response) => {
                 if (!response.status === 200) {
--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,4 @@
 export const MOVIES_API_URL = 'http://www.mocky.io/v2/5a6ba76531000008301b8b59';
-export const MOVIES_API_URL_2 = 'http://www.mocky.io/v2/5a8ca2243000006d00323fb2';
-export const MOVIES_API_URL_3 = 'http://www.mocky.io/v2/5a8ca2243000006d00323fb2';
 
 export const FACEBOOK_APP = {
     ID: '370859849993011',
